dad7141c992bf99f1468515199007f5d5e46ed7e
Yemata Ferry <yemataferry@gmail.com>
2024-01-06 09:29:26 +0100

    linter corrections

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,36 +4,36 @@ const empties = document.querySelectorAll('.empty')
 fill.addEventListener('dragstart', dragStart)
 fill.addEventListener('dragend', dragEnd)
 
-for(const empty of empties) {
+for (const empty of empties) {
   empty.addEventListener('dragover', dragOver)
   empty.addEventListener('dragenter', dragEnter)
   empty.addEventListener('dragleave', dragLeave)
   empty.addEventListener('drop', dragDrop)
 }
 
-function dragStart() {
+function dragStart () {
   this.className += ' hold' 
-  setTimeout(() => this.className = 'invisible', 0)
+  setTimeout(() => {this.className = 'invisible'}, 0)
 }
 
-function dragEnd() {
+function dragEnd () {
   this.className = 'fill'
 }
 
-function dragOver(e) {
+function dragOver (e) {
   e.preventDefault()
 }
 
-function dragEnter(e) {
+function dragEnter (e) {
   e.preventDefault()
   this.className += ' hovered'
 }
 
-function dragLeave() {
+function dragLeave () {
   this.className = 'empty'
 }
 
-function dragDrop() {
+function dragDrop () {
   this.className = 'empty'
   this.append(fill)
 }
